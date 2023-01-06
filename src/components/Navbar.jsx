23bfdce5c7fcb845154e55775b104b603919092d
ArlindPestisha <arlind.pestisha@hyperisland.se>
2023-01-06 16:50:31 +0100

    init the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stack } from "@mui/system";
+import { Link } from "react-router-dom";
+import {logo} from '../utils/constants'
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
